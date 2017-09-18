c46ff9be46040dde084a7401348f1aea29275ad2
Ushinji <siguma971@gmail.com>
2017-09-18 22:20:34 +0900

    Ruby on Rails Tutorial

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,5 +13,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'users/show'
     assert_not !flash[:success]
+    assert is_logged_in?
   end
 end
