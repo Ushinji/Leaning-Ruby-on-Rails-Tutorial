6975eb9292b795b9fa986f8e7c3782c21d4b659d
Ushinji <siguma971@gmail.com>
2017-10-02 06:48:19 +0900

    Add micropost view

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -17,4 +17,14 @@ class UserTest < ActiveSupport::TestCase
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?('')
   end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content:"Lorem jpsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
+
+
 end
