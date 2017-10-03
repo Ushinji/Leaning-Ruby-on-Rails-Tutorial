7d44cb5fd289d8a0792774250c770f77d620ee26
Ushinji <siguma971@gmail.com>
2017-10-03 22:54:42 +0900

    Add Micropost create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,14 +52,6 @@ private
   end
 
   # beforアクション
-  # ログイン済みユーザかどうか確認
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in!"
-      redirect_to login_url
-    end
-  end
 
   # 正しいユーザかどうか確認
   def correct_user
